7561aab0addaa6c4748a9bb6b0d9a9950e2b8622
Josh Hackney <jhaxllc@gmail.com>
2022-07-14 15:17:18 -0400

    added some skeleton code, will check this commit later to make a template

--- main.go
+++ main.go
@@ -2,19 +2,12 @@ package main
 
 import (
 	"fmt"
+
+	"gitlab.com/jhax/goplayground/twosum/maptest"
 )
-
-func mapPractice() string {
-	var ret string
-
-	return ret
-}
 
 func main() {
 	fmt.Println("Starting")
-	var out string
-
-	out = mapPractice()
-
+	out := maptest.MapPractice()
 	fmt.Printf("Output below:\n%s", out)
 }
---
+++ twosum/maptest/maptest.go
@@ -0,0 +1,8 @@
+package maptest
+
+func MapPractice() (string, error) {
+	var err error
+	var ret string
+
+	return ret, err
+}
